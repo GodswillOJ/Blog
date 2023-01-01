12a242d79990fc34a817368399f3619f50ba48d0
GodswillOJ <110397088+GodswillOJ@users.noreply.github.com>
2023-01-01 22:12:53 +0100

    my blog

---
+++ public/script/home.js
@@ -0,0 +1,7 @@
+const logoid = document.getElementsByClassName("hero_section");
+
+for(image of someImages){
+    image.src = "https://images.pexels.com/photos/13747843/pexels-photo-13747843.jpeg?cs=srgb&dl=pexels-james-gana-13747843.jpg&fm=jpg&_gl=1*wukxkt*_ga*MjA5NTkxMDg2LjE2NjMzNTc3NDM.*_ga_8JE65Q40S6*MTY2ODE3Mjc4NC4zLjEuMTY2ODE3NDE3Ny4wLjAuMA..";
+}
+//<!-- <% if(isLoggedIn === true) { %> log out <% }else{ %> log in <% }%> </a></p> -->
+
---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const data = require('./database/data.json');
+const PORT = 5000;
+
+app.set("view engine", "ejs");
+app.set("views", "frontend");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', function (req, res) {
+    console.log(data);
+    const username ="Godswill";
+    const isLoggedIn = true;
+    //const posts = data[0].posts;
+    //console.log(posts);
+    //res.render("home_page.ejs", {posts})
+    
+    const allPosts = [];
+    for (let category of data) {
+        for (let post of category.posts){
+            allPosts.push(post); }
+        }
+        
+// git clone repo_url
+// To update your git repo from your terminal, you need the following commands
+// git add .
+// git commit -m "your commit message goes here"
+// git push origin main
+        res.render("home_page.ejs", {allPosts} );
+    // app.get('/createpost', function (req, res){
+        //     const date = new Date().toDateString();
+        //     console.log(date);
+        // })
+        
+        
+        // app.get("/", function (req, res){
+            //    res.render("home_page.ejs");
+            //    console.log(__dirname);
+});
+
+
+
+app.get("/signin",function (req, res) {
+    res.render("signin.ejs")
+} );
+app.get("/signup",function (req, res) {
+    res.render("signup.ejs")
+} );
+app.get("/post",function (req, res) {
+    res.render("post.ejs")
+} );
+//install npm install path
+app.listen(PORT, function () {
+    console.log(`Server started at ${PORT}`)
+});
